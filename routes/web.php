c9d0ea43dab4dfd02a2ac9a2dafc516d9496740d
cedecj042 <cedricoporto1st@gmail.com>
2024-06-20 11:50:06 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register',[AuthController::class,'showRegistrationForm'])->name('register');
+Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/login',[AuthController::class,'showLoginForm'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+
+Route::get('/logout',[AuthController::class,'logout']);
+Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/home','homepage');
 });
